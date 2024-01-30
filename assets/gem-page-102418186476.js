dc1ab74e10d01cfd11dd5bd19bc7c0fa41d46121
Vladyslav Klymenko <116367534+torokhtiysupport@users.noreply.github.com>
2024-01-30 10:17:34 +0200

    Init

---
+++ assets/gem-page-102418186476.js
@@ -0,0 +1,8 @@
+
+
+    
+(function( jQuery ){
+  // var $module = jQuery('#m-1663494642247').children('.module');
+  // You can add custom Javascript code right here.
+})( window.GemQuery || jQuery );
+    
